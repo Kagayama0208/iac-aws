acd99e51bb49b0e6bde2aa686dd99cdc9a824be3
Kagayama0208 <kousuke_k@icloud.com>
2026-06-01 14:31:41 +0900

    github actionsのyml追加

--- variables.tf
+++ variables.tf
@@ -3,12 +3,6 @@ variable "aws_region" {
   default = "ap-northeast-1"
 }
 
-variable "aws_account_id" {
-  type        = string
-  description = "AWS account ID. Set via TF_VAR_aws_account_id env var or tfvars."
-  sensitive   = true
-}
-
 variable "keiba_db_backup_bucket_name" {
   type        = string
   description = "S3 bucket name for keiba-db CNPG backup"

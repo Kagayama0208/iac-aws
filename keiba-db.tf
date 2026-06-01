0dcf1f4abd859b7eba159a34e20c7e8f8ba5a6cb
Kagayama0208 <kousuke_k@icloud.com>
2026-06-01 17:21:45 +0900

    fix: terraform init -upgrade を実行して v6 プロバイダ取得 + .terraform.lock.hcl 更新 bootstrap/02-github-oidc.sh を実行して OIDC ロールに IAM + KMS 権限付与

--- keiba-db.tf
+++ keiba-db.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "keiba-db-backup" {
-  bucket = var.keiba_db_backup_bucket_name
+  bucket           = var.keiba_db_backup_bucket_name
+  bucket_namespace = "global"
 
   tags = {
     Project = "keiba-db"
